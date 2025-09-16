fe7d3f99586399e6cff3c75c2301c5c2a444a5bd
Prince Lee <prince_lee.nna85@ymail.com>
2025-09-16 13:32:47 +0100

    chores: add cloud backend

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -2,4 +2,4 @@
 
 
 //export const baseUrl = "http://localhost:3002";
-export const baseUrl = "https://backend-school-management-system.onrender.com";
+export const baseUrl = "https://backend-school-management-system-1.onrender.com";
