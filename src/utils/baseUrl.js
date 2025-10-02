1ed639a5c476b5c953f58ee861101cdf67f34d4e
Prince Lee <prince_lee.nna85@ymail.com>
2025-10-02 11:55:20 +0100

    change base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -2,5 +2,5 @@
 
 
   // export const baseUrl = "http://localhost:3002";
-  export const baseUrl = "https://backend-school-management-system.onrender.com";
+  export const baseUrl = "https://princelee-school-management-backend.onrender.com";
   //checking...
